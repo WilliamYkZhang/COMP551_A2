7022664b0cb267de3a888ae69e32e50776591dc8
Er Jun Li <er.li@mail.mcgill.ca>
2019-10-10 13:14:01 -0400

    Minor changes

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,6 @@ def reddit_preprocess():
 
     # Apply stemming function 
     df["stemmed"] = df["comments"].apply(stem)
-    # print(df["stemmed"])
 
     # Split feature and targ(et variables 
     X = df["stemmed"] # pandas series
@@ -24,16 +23,12 @@ def reddit_preprocess():
     x_train, x_valid, y_train, y_valid = train_test_split(X, y, train_size = 0.8, test_size = 0.2, shuffle=True)
 
     vectorized_x_train = vectorize(x_train)
-    # Only transform not fit )
-    # vectorized_x_test = vectorize(x_valid)
+    # vectorized_x_test = vectorize(x_valid) Only transform not fit )
 
-    # print(x_train)
     print(vectorized_x_train)
 
-    # Get rid of &gt
-    # print(x_train)
-# Remove stop-words stem function 
-# PCA 
+    # TODO: Get rid of &gt and Youtube links
+    # TODO: Implement Regularization (i.e. PCA) 
 
 def vectorize(training_data):
     """ Vectorize text using TfidfVectorizer """
@@ -41,7 +36,7 @@ def vectorize(training_data):
     stop_words = set(stopwords.words('english'))
 
     # Create a tf_idf word vectorizer 
-    tf_idf_vectorizer = TfidfVectorizer(decode_error = 'ignore', strip_accents='unicode', stop_words=stop_words)
+    tf_idf_vectorizer = TfidfVectorizer(decode_error = 'ignore', strip_accents='unicode', stop_words=stop_words, analyzer = "word")
     
     return tf_idf_vectorizer.fit_transform(training_data)
     
@@ -57,16 +52,16 @@ if __name__ == "__main__":
     # from nltk import download
 
     # download the "all-corpora" corpus
-    # download()
-    reddit_preprocess()
+    # download() # only need to be executed once in order to download language packages from nltk
+    # reddit_preprocess()
     
     # Testing different Stemming functions 
-    # from nltk.stem.snowball import SnowballStemmer # There are several stemmers that you can use 
+    from nltk.stem.snowball import SnowballStemmer # There are several stemmers that you can use 
 
-    # # Declare language I want to stem in
-    # stemmer = SnowballStemmer("english")
-    # stemmer.stem("responsiveness")
-    # stemmer.stem("responsivity")
-    # stemmer.stem("unresponsive") # there are some restriction to this stemmer as it didn't strip the "un"
+    # Declare language I want to stem in
+    stemmer = SnowballStemmer("english")
+    stemmer.stem("responsiveness")
+    stemmer.stem("responsivity")
+    stemmer.stem("unresponsive") # there are some restriction to this stemmer as it didn't strip the "un"
 
 
