e3a2cc11f97077175cbcaee60d755134a207efab
Er Jun Li <er.li@mail.mcgill.ca>
2019-10-17 20:07:58 -0400

    Validating models

--- model_selection.py
+++ model_selection.py
@@ -1,25 +1,94 @@
+# Preprocessing
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+from nltk.corpus import stopwords
+import pandas as pd
+
+# Transformers 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+# Models 
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC 
+import xgboost as xgb
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
 
 """ Module containing the model selection pipeline """
-Class ModelSelector:
-    def __init__(self):
-        self.pipeline = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', SGDClassifier(tol=1e-3)),])
-        self.parameters = parameters = {
-                                        'vect__max_df': (0.5, 0.75, 1.0),
-                                        # 'vect__max_features': (None, 5000, 10000, 50000),
-                                        'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
-                                        # 'tfidf__use_idf': (True, False),
-                                        # 'tfidf__norm': ('l1', 'l2'),
-                                        'clf__max_iter': (20,),
-                                        'clf__alpha': (0.00001, 0.000001),
-                                        'clf__penalty': ('l2', 'elasticnet'),
-                                        # 'clf__max_iter': (10, 50, 80),
-                                        }
-def validate(model, features, target):
-    return cross_val_score(model, X=features, y=target)
-    # we need to transform each fold and train on every n-1 folds. 
+if __name__  == "__main__":
 
-def grid_search_cv(model, params):
-    return GridSearchCV(model, param_grid = params)
+    # Get a list of stopwords
+    stopwords = stopwords.words("english")
+
+    # Transformers 
+    c_vect = CountVectorizer(lowercase=True, encoding="utf-8", decode_error="ignore", strip_accents='unicode',stop_words=stopwords, analyzer = "word")
+    tfidf_vect = TfidfVectorizer(lowercase=True, encoding = "utf-8",  decode_error = 'ignore', strip_accents='unicode', stop_words=stopwords, analyzer = "word")  
+    svd = TruncatedSVD()
+
+    # Estimators 
+    log_reg = LogisticRegression()
+    svc = SVC() # class weight , experiement values 
+    xgb = xgb.XGBClassifier()
+    decision_tree_clf = DecisionTreeClassifier()
+    rff = RandomForestClassifier()
+    multi_NB = MultinomialNB()
+    
+    # Building pipeline 
+    pipeline_cvect = Pipeline([('cvect', c_vect),('svd', svd),('clf', multi_NB),], memory="Pipeline Output/", verbose=True)
+    pipeline_tfidf = Pipeline([('tfidf', tfidf_vect), ('clf', multi_NB)], memory="Pipeline Output/", verbose=True)
+    pipeline_cvect_tfidf = Pipeline([('cvect', c_vect),('tfidf', tfidf_vect),('svd', svd),('clf', multi_NB),], memory="Pipeline Output/", verbose=True)
+    # ('svd', svd),
+    # Instantiate parameters for pipeline     
+    parameters_cvect = {
+        'cvect__max_df': (0.5, 0.75, 1.0),
+        'cvect__max_features': (None, 5000, 10000, 50000),
+        'cvect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+        'svd__n_components' : [2,50,100,250, 500,1000,10000,25000],
+        'svd__algorithm': ("arpack", "randomized"),
+        }
+
+    parameters_tfidf = {
+        'tfidf__max_features': (None, 10000, 25000, 50000),
+        'tfidf__use_idf': (True, False), # Enable inverse-document-frequency reweighting.
+        'tfidf__max_df': (0.5, 0.75, 0.9), # ignore terms that have a document frequency strictly higher than the given threshold
+        'tfidf__min_df': (0.025, 0.05, 0.1), #  ignore terms that have a document frequency strictly lower than the given threshold
+        'tfidf__norm': ('l1', 'l2', None), # regularization term
+        'tfidf__smooth_idf': (True, False), # Smooth idf weights by adding one to document frequencies, as if an extra document was seen containing every term in the collection exactly once.Prevents zero divisions
+        'tfidf__ngram_range': ((1, 1), (1, 2)), # n-grams to be extracted
+        # 'svd__n_components' : (10, 500,1000,2500, 5000, 7500, 10000),
+        # 'svd__algorithm': ("arpack", "randomized"),
+        'clf__alpha': (0.25, 0.5, 0.75),
+        'clf__fit_prior': (True, False),      
+    }  
+
+    parameters_cvect_tfidf = {
+        'tfidf__max_features': (None, 5000, 10000, 50000),
+        'tfidf__use_idf': (True, False),
+        'tfidf__norm': ('l1', 'l2', None),
+        'tfidf__ngram_range': ((1, 1), (1, 2)), 
+        'svd__n_components' : [2,50,100,250, 500,1000,10000,25000],
+        'svd__algorithm': ("arpack", "randomized"),
+        'clf__max_iter': (20,),
+        'clf__alpha': (0.00001, 0.000001),
+        'clf__penalty': ('l2', 'elasticnet'),
+        'clf__max_iter': (10, 50, 80),        
+    }  
+
+    # Read DataFrame
+    stemmed_df = pd.read_csv("preprocessed_reddit_train_SnowballStemmer.csv")
+    lemmatized_df = pd.read_csv("preprocessed_reddit_train_WordNetLemmatizer.csv")
+
+    # Separate X and Y 
+    X_stem = stemmed_df["cleaned"]
+    y_stem = stemmed_df["label"]
+    X_lemma = lemmatized_df["cleaned"]
+    y_lemma = lemmatized_df["label"]
+    
+    # Check for number of features.
+    gs_CV = GridSearchCV(pipeline_tfidf, param_grid = parameters_tfidf, cv=5)
+    gs_CV.fit(X_lemma, y_lemma)
+    print("Best parameter (CV score={0:.3f}):".format(gs_CV.best_score_))
+    print(gs_CV.best_params_)
--- preprocess.py
+++ preprocess.py
@@ -21,24 +21,29 @@ import string
 """ Class containing preprocessing function for reddit comments """
 class Preprocessor: 
 
-    def __init__(self):
+    def __init__(self, normalizer):
         self.label_encoder = LabelEncoder()
-        self.tf_idf_vectorizer = TfidfVectorizer(lowercase=True,  decode_error = 'ignore', strip_accents='unicode', stop_words=set(stopwords.words('english')), analyzer = "word")  
+        self.tf_idf_vectorizer = TfidfVectorizer(lowercase=True, encoding = "utf-8",  decode_error = 'ignore', strip_accents='unicode', stop_words=set(stopwords.words('english')), analyzer = "word")  
+        if normalizer == "stemmer": 
+            self.normalizer = SnowballStemmer("english")
+        elif normalizer == "lemmatizer":
+            self.normalizer = WordNetLemmatizer()
+        else:
+            raise Exception("Normalizer must be \"stemmer\" or \"lemmatizer\".")        
             
     def preprocess_reddit_train(self):
         """ Stores a cleaned up version of the dataset on the current directory """
         # Read dataset
         df = pd.read_csv("reddit_train.csv")
         # Apply stemming function 
-        df["cleaned"] = df["comments"].apply(Preprocessor.clean_text)
+        df["cleaned"] = df["comments"].apply(self.clean_text)
         # Transform each subreddit into an unique integer
         df["label"] = self.label_encoder.fit_transform(df["subreddits"])
         # Save cleaned dataset
-        df.to_csv("preprocessed_reddit_train.csv",index=False)       
-        # TODO: Implement Regularization (i.e. PCA) 
+        df.to_csv("preprocessed_reddit_train_" + type(self.normalizer).__name__ + ".csv",index=False)       
+        # TODO: Implement Regularization (i.e. PCA, SVD, L1, L2...?) 
 
-    @staticmethod
-    def clean_text(sentence): 
+    def clean_text(self, sentence): 
         # Put all words to lower case 
         sentence = sentence.lower()    
         # Tokenize words   
@@ -46,34 +51,44 @@ class Preprocessor:
         # Remove punctuation 
         word_tokens = [_ for _ in word_tokens if _ not in string.punctuation]
         # Remove non-alphabetical char 
-        word_tokens = [re.sub(pattern="[^a-zA-Z0-9\s]", repl= "",string=_) for _ in word_tokens]
+        word_tokens = [re.sub(pattern="[^a-zA-Z0-9\s]", repl= "", string = _ ) for _ in word_tokens]
         # Remove empty strings 
         word_tokens = [_ for _ in word_tokens if _]
         # Stem words
-        processed_sentence = Preprocessor.stem(" ".join(word_tokens))
+        processed_sentence = self.normalize(" ".join(word_tokens))
         # TODO: Remove links?
         return processed_sentence
     
-    @staticmethod
-    def stem(sentence):
-        stemmed_str = []
+    def normalize(self, sentence):
+        normalized_str = []
         word_tokens = word_tokenize(sentence)
-        stemmer = SnowballStemmer("english")
-        for i in word_tokens:
-            stemmed_str.append(stemmer.stem(i))
-        return " ".join(stemmed_str)
+        if type(self.normalizer).__name__ == "SnowballStemmer": 
+            for i in word_tokens:
+                normalized_str.append(self.normalizer.stem(i))
+        elif type(self.normalizer).__name__ == "WordNetLemmatizer":
+            for i in word_tokens:
+                normalized_str.append(self.normalizer.lemmatize(i))        
+        else:
+            raise Exception("Normalizer must be \"stemmer\" or \"lemmatizer\".")
+
+        return " ".join(normalized_str)
     
     def preprocess_reddit_test(self):
         """ Returns a cleaned up version of the test dataset """
         # Read dataset
         df = pd.read_csv("reddit_test.csv")
         # Apply stemming function 
-        df["cleaned"] = df["comments"].apply(Preprocessor.clean_text)
+        df["cleaned"] = df["comments"].apply(self.clean_text)
         # Store cleaned test set in 
-        df.to_csv("preprocessed_reddit_test.csv", index=False)      
+        df.to_csv("preprocessed_reddit_test_" + type(self.normalizer).__name__  + ".csv", index=False)      
 
 if __name__ == "__main__":  
+ 
+    # Ways to normalize
+    lemmatize = "lemmatizer"
+    stem = "stemmer"
+
     # Create a preprocessed training and test csv file  
-    preprocessor = Preprocessor()
+    preprocessor = Preprocessor(stem)
     preprocessor.preprocess_reddit_train()
     preprocessor.preprocess_reddit_test()
