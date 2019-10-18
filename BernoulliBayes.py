24fdba7e4b7958fa16018a2e9449e0c143ccb155
KartoffelKopf <ethan.anderson995@gmail.com>
2019-10-17 21:00:40 -0400

    working naive bernoulli bayes

--- BernoulliBayes.py
+++ BernoulliBayes.py
@@ -1,6 +1,11 @@
 import numpy as np
+import pandas as pd
 from sklearn import preprocessing
-from scipy.sparse import csc_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import word_tokenize
 
 
 class BernoulliBayes:
@@ -52,3 +57,52 @@ class BernoulliBayes:
             predictions.append(np.argmax(example))
 
         print("accuracy: " + str(np.sum(predictions == validationLabels)/len(predictions)))
+
+
+def preprocess():
+    # Read dataset
+    df = pd.read_csv("reddit_train.csv")
+
+    # Apply stemming function
+    df["stemmed"] = df["comments"].apply(stem)
+
+    # Transform each subreddit into an unique integer
+    labels, levels = pd.factorize(df["subreddits"])
+    df["labels"] = pd.Series(labels)
+
+    # # Split feature and targ(et variables
+    X = df["stemmed"]  # pandas series
+    y = df["labels"]  # pandas series
+
+    # Split training and validation set
+    x_train, x_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, shuffle=True)
+
+    # Vectorize training data
+
+    vectorized_x_train, vectorized_x_valid = bernoulli_vectorize(x_train, x_valid)
+    # Only transform not fit
+    return vectorized_x_train, y_train, vectorized_x_valid, y_valid
+
+def stem(sentence):
+    stemmed_str = []
+    word_tokens = word_tokenize(sentence)
+    stemmer = SnowballStemmer("english")
+    for i in word_tokens:
+        stemmed_str.append(stemmer.stem(i))
+    return " ".join(stemmed_str)
+
+def bernoulli_vectorize(training_data, validation_data):
+    """ Vectorize text using CountVectorizer """
+    # Get a list of stopwords
+    stop_words = set(stopwords.words('english'))
+
+    # Create a hashing word vectorizer
+    count_vectorizer = CountVectorizer(decode_error='ignore', strip_accents='unicode', stop_words=stop_words, analyzer="word", binary=True)
+
+    return count_vectorizer.fit_transform(training_data), count_vectorizer.transform(validation_data)
+
+if __name__ == "__main__":
+    test = BernoulliBayes(0.5)
+    testSet, testLabels, validSet, validLabels = preprocess()
+    test.fit(testSet, testLabels)
+    test.validate(validSet, validLabels)
